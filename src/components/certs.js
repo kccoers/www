4606f242d0060edb634061fb5c6a15fac4669608
Kevin Coers <coers.kevin@gmail.com>
2023-08-13 18:59:49 -0500

    updated certs page

--- src/components/certs.js
+++ src/components/certs.js
@@ -4,23 +4,60 @@ function Certs() {
     const certs = certifications.certs;
 
     return(
-        <div className='certsContainer'>
-        {certs.map((cert) => {
-            const image = require(`../images/${cert.imageFile}`)
+        <>
+        <div className='certifications'>
+            <div className='certsCategory'>Networking</div>
+            <div className='certsContainer'>
+            {certs.networking.map((cert) => {
+                const image = require(`../images/${cert.imageFile}`)
 
-            return(
-                <div className='certCard'>
-                    <div className='certCardImg'>
-                        <img src={image} />
+                return(
+                    <div className='certCard'>
+                        <div className='certCardImg'>
+                            <img src={image} />
+                        </div>
+                        <div className='certCardInfo'>
+                            <p className='header'>{cert.name} ({cert.abbr})</p>
+                            { cert.hyperlink.length > 0 &&
+                                <p className='info'>
+                                    <a href={cert.hyperlink} target='blank'>{cert.certNumber}</a>
+                                </p>
+                            }
+                            { cert.hyperlink.length == 0 &&
+                                <p className='info'>{cert.certNumber}</p>
+                            }
+                        </div>
                     </div>
-                    <div className='certCardInfo'>
-                        <p className='header'>{cert.name} ({cert.abbr})</p>
-                        <p className='info'>{cert.certNumber}</p>
+                )
+            })}
+            </div>
+            <div className='certsCategory'>Cloud</div>
+            <div className='certsContainer'>
+            {certs.cloud.map((cert) => {
+                const image = require(`../images/${cert.imageFile}`)
+
+                return(
+                    <div className='certCard'>
+                        <div className='certCardImg'>
+                            <img src={image} />
+                        </div>
+                        <div className='certCardInfo'>
+                            <p className='header'>{cert.name} ({cert.abbr})</p>
+                            { cert.hyperlink.length > 0 &&
+                                <p className='info'>
+                                    <a href={cert.hyperlink} target='blank'>{cert.certNumber}</a>
+                                </p>
+                            }
+                            { cert.hyperlink.length == 0 &&
+                                <p className='info'>{cert.certNumber}</p>
+                            }
+                        </div>
                     </div>
-                </div>
-            )
-        })}
+                )
+            })}
+            </div>
         </div>
+        </>
     )
 }
 
