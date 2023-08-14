d9b254647c5fd41bbb78543f0d7bc5cf5198bd72
Kevin Coers <coers.kevin@gmail.com>
2023-08-13 20:32:35 -0500

    updated about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,25 @@
 function About() {
     return(
-        <>About</>
+        <div className="aboutContainer">
+            <h2>Professional</h2>
+            <p>Technical Solutions Architect with over 10 years of experience 
+                and a strong affinity towards providing solutions and products 
+                based on business needs, return on investment, scalability, 
+                and technical debt elimination. Proven success in strategically 
+                aligning IT Solutions and products with IT and Business Operations.
+            </p>
+            <hr />
+            <h2>Areas of Experties</h2>
+            <p>Solutions Architecture | Network Architecture & Design | Cisco Meraki Firewalls | 
+                Cisco Meraki Switches | Cisco Meraki Wireless | Wireless Engineering | 
+                Ekahau Wireless Design Tools | Amazon Web Services | Collaboration Tools | 
+                Cisco Webex | Azure AD | Active Directory | Windows Server Administration | 
+                Linux Server Administration | Powershell | API
+            </p>
+            <hr />
+            <h2>Hobbies</h2>
+
+        </div>
     )
 }
 
