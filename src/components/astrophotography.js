eb8c2794d0b4838e78408effa04f558fa2613ed7
Kevin Coers <coers.kevin@gmail.com>
2024-11-02 15:20:06 -0500

    Added noreferrer rel and alt prop to images

--- src/components/astrophotography.js
+++ src/components/astrophotography.js
@@ -27,11 +27,17 @@ function Astrophotography() {
             albums.map(album => (
                 <Row>
                     <h3 style={{textTransform:"capitalize"}}>Album: {album.albumName}</h3>
-                    <Carousel data-bs-theme="dark" style={{background:"rgba(175, 175, 175, 0.5)",borderRadius:".5rem"}}>
+                    <Carousel
+                        data-bs-theme="dark"
+                        style={{background:"rgba(175, 175, 175, 0.5)",borderRadius:".5rem"}}
+                    >
                     {album.photos.map(photo => (
                         <Carousel.Item style={{textAlign:"center"}}>
-                            <a href={photo.photoUrl} target='_blank'>
-                                <img src={photo.photoUrl} style={{margin:"6rem 0",height:"20rem"}} />
+                            <a href={photo.photoUrl} target="_blank" rel="noreferrer">
+                                <img
+                                    src={photo.photoUrl}
+                                    style={{margin:"6rem 0",height:"20rem"}}
+                                    alt={photo.photoUrl} />
                             </a>
                         </Carousel.Item>
                     ))}
