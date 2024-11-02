e7b12ac94657a3ffcb2535cbaf9a8abf04dc2ab7
Kevin Coers <coers.kevin@gmail.com>
2024-11-02 15:14:03 -0500

    Added carousel to astrophotography component

--- src/components/astrophotography.js
+++ src/components/astrophotography.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Button,
-         Card,
-         Carousel,
-         Col,
+import { Carousel,
          Container,
-         Row,
-         Spinner
+         Row
 } from 'react-bootstrap';
 
 function Astrophotography() {
@@ -25,19 +21,6 @@ function Astrophotography() {
         fetchAlbumData()
     }, [])
 
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
-    };
-
-    /*<div style={{textAlign:"center",margin:"0 auto"}}>
-        <p>Background Image Credit:&nbsp;
-            <a style={{color: '#FFF'}} href="https://webbtelescope.org/contents/media/images/2023/128/01H449193V5Q4Q6GFBKXAZ3S03?news=true" target="_blank" rel="noreferrer">Webb Space Telescope</a>
-        </p>
-    </div>*/
-
-
     return(
         <Container style={{marginTop:"1rem"}}>
         {albums.length > 0 && (
@@ -56,40 +39,6 @@ function Astrophotography() {
                 </Row>
             ))
         )}
-        {/*albums.length > 0 && (
-            <Row>
-                {albums.map(album => (
-                    <>
-                    <Col key={album.id} xs={12} m={12} lg={12}>
-                        <Card style={{marginRight:"1rem",marginBottom:"1rem",border:"none",width:"fit-content"}}>
-                            <Card.Img
-                                variant='top'
-                                src="https://kccoers.github.io/astrophotography/albums/starscapes/20231015_005411155_iOS.webp"
-                                style={{height:"20rem"}} />
-                            <Card.Body>
-                                <Card.Title>{album.albumName}</Card.Title>
-                                <Card.Text>{album.albumDescription}</Card.Text>
-                                <Button variant='outline-dark' href="https://kccoers.github.io/astrophotography/data.json" target="_blank">VIEW ALBUM</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col key={album.id} xs={12} m={6} lg={4}>
-                        <Card style={{marginRight:"1rem",marginBottom:"1rem",border:"none",width:"fit-content"}}>
-                            <Card.Img
-                                variant='top'
-                                src={album.albumCover}
-                                style={{height:"20rem"}} />
-                            <Card.Body>
-                                <Card.Title>{album.albumName}</Card.Title>
-                                <Card.Text>{album.albumDescription}{album.albumDescription}{album.albumDescription}{album.albumDescription}{album.albumDescription}</Card.Text>
-                                <Button variant='outline-dark' href="https://kccoers.github.io/astrophotography/data.json" target="_blank">VIEW ALBUM</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    </>
-                ))}
-            </Row>
-        )*/}
         </Container>
     )
     
